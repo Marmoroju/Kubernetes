600220a85411beee45a644809d2149522c018873
Marmoroju <marcosmouraorodriguesjr@gmail.com>
2024-01-06 16:46:26 -0300

    vagrant-k8s-kind-minukube-docker-ubuntu20-04

---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+Vagrant.configure("2") do |config|
+    config.vm.define "k8s" do |vm_k8s|
+        vm_k8s.vm.box = "ubuntu/focal64"
+        vm_k8s.vm.network "private_network", ip: "192.168.56.5"
+        vm_k8s.vm.hostname = "k8s"
+        vm_k8s.vm.provision "shell", path: "provision.sh"            
+        vm_k8s.vm.provider "virtualbox" do |v|
+            v.memory = 2048
+            v.cpus = 2
+        end
+    end
+end
---
+++ provision.sh
@@ -0,0 +1,31 @@
+#/bin/sh
+
+#Instalção do Kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+#Instalação do Kind
+curl -Lo ./kind curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+
+chmod +x ./kind
+
+sudo mv ./kind /usr/local/bin/kind
+
+#Instalção do Minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+chmod +x ./minikube
+
+sudo mv ./minikube /usr/local/bin/minikube
+
+minikube version
+
+#Instalção do Docker
+curl -fsSL https://get.docker.com | bash
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
