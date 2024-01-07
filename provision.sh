29557a0bbf6250708532b33e3db7d10ceab0ec55
Marmoroju <marcosmouraorodriguesjr@gmail.com>
2024-01-07 11:17:49 -0300

    update de criação do cluster kind

--- provision.sh
+++ provision.sh
@@ -1,8 +1,13 @@
 #/bin/sh
 
-sudo apt-get -y update && upgrade && clean
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
-#Instalção do Kubectl
+#Instalação do Kubectl
+echo '###############################'
+echo 'Instalação do Kubectl'
+echo '###############################'
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 
 chmod +x ./kubectl
@@ -10,13 +15,21 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 #Instalação do Kind
+echo '###############################'
+echo 'Instalação do kind'
+echo '###############################'
+
 curl -Lo ./kind curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 
 chmod +x ./kind
 
 sudo mv ./kind /usr/local/bin/kind
 
-#Instalção do Minikube
+#Instalação do Minikube
+echo '###############################'
+echo 'Instalação do Minikube'
+echo '###############################'
+
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 
 chmod +x ./minikube
@@ -25,9 +38,25 @@ sudo mv ./minikube /usr/local/bin/minikube
 
 minikube version
 
-#Instalção do Docker
+#Instalação do Docker
+echo '###############################'
+echo 'Instalação do Docker'
+echo '###############################'
+
 curl -fsSL https://get.docker.com | bash
 
 sudo systemctl start docker
 
 sudo systemctl enable docker
+
+#Criação de cluster através do Kind
+echo '###############################'
+echo 'Criação do cluster'
+echo '###############################'
+
+sudo kind create cluster
+
+
+
+
+
