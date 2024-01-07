7baef955c7ba7894b20e76b9bee3d197ca00c5de
Marmoroju <marcosmouraorodriguesjr@gmail.com>
2024-01-07 08:52:49 -0300

    update provision

--- provision.sh
+++ provision.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+sudo apt-get -y update && upgrade && clean
+
 #Instalção do Kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 
